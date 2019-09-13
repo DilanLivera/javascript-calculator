b41c893a207302b371b95198226d63816fbfa6df
Dilan Livera <dylan_yahampath@yahoo.com>
2019-09-13 18:15:22 +1000

    add event handlers

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-function Key({ id, text }) { 
+function Key({ id, text, onClick }) { 
   return (
-    <button id={ id } className="Key" >{ text }</button>
+    <button id={ id } className="Key" onClick={ onClick }>{ text }</button>
   );
 }
 
@@ -10,49 +10,72 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state= {
-      left: 2,
-      right: 4,
-      operator: "x",
-      output: 8
+      calculation: "0",
+      output: 0
     }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClearClick = this.handleClearClick.bind(this);
+    this.handleOperatorClick = this.handleOperatorClick.bind(this);
   }
+
+  handleClick(evt) {
+    let { calculation, output } = this.state;
+    calculation = (calculation === "0") ? evt.target.innerText : calculation + evt.target.innerText;
+    this.setState({ calculation, output });
+  }
+
+  handleClearClick() {
+    let calculation = "0";
+    let output = 0;
+    this.setState({ calculation, output });
+  }
+
+  handleOperatorClick(evt) {
+    const operator = evt.target.innerText;
+    if(operator === "+") console.log(operator);
+    if(operator === "-") console.log(operator); 
+    if(operator === "x") console.log(operator); 
+    if(operator === "/") console.log(operator);
+    if(operator === "=") console.log(operator);  
+  }
+
   render() {
-    const { left, right, operator, output } = this.state;
-    let calculation = left+operator+right;
+    const { calculation, output } = this.state;
+
     return (
       <div className="Calculator">
         <div className="Calculator-display">
           <h3 className="Calculator-calculation">{ calculation }</h3>
-          <h3 className="Calculator-output">{ output }</h3>
+          <h3 className="Calculator-output" id="display">{ output }</h3>
         </div>
         <table>
           <tbody>
           <tr>
-            <td colSpan="2" ><Key id="ac" text="AC" /></td>
-            <td><Key id="devide" text="/" /></td>
-            <td><Key id="multiply" text="x" /></td>
+            <td colSpan="2" ><Key id="clear" text="clear" onClick={ this.handleClearClick } /></td>
+            <td><Key id="devide" text="/" onClick={ this.handleOperatorClick } /></td>
+            <td><Key id="multiply" text="x" onClick={ this.handleOperatorClick } /></td>
           </tr>
           <tr>
-            <td><Key id="seven" text="7" /></td>
-            <td><Key id="eight" text="8" /></td>
-            <td><Key id="nine" text="9" /></td>
-            <td><Key id="substract" text="-" /></td>
+            <td><Key id="seven" text="7" onClick={ this.handleClick } /></td>
+            <td><Key id="eight" text="8" onClick={ this.handleClick } /></td>
+            <td><Key id="nine" text="9" onClick={ this.handleClick } /></td>
+            <td><Key id="substract" text="-" onClick={ this.handleOperatorClick } /></td>
           </tr>
           <tr>
-            <td><Key id="four" text="4" /></td>
-            <td><Key id="five" text="5" /></td>
-            <td><Key id="six" text="6" /></td>
-            <td><Key id="add" text="+" /></td>
+            <td><Key id="four" text="4" onClick={ this.handleClick } /></td>
+            <td><Key id="five" text="5" onClick={ this.handleClick } /></td>
+            <td><Key id="six" text="6" onClick={ this.handleClick } /></td>
+            <td><Key id="add" text="+" onClick={ this.handleOperatorClick } /></td>
           </tr>
           <tr>
-            <td><Key id="one" text="1" /></td>
-            <td><Key id="two" text="2" /></td>
-            <td><Key id="three" text="3" /></td>
-            <td rowSpan="2" ><Key id="equal" text="=" /></td>
+            <td><Key id="one" text="1" onClick={ this.handleClick } /></td>
+            <td><Key id="two" text="2" onClick={ this.handleClick } /></td>
+            <td><Key id="three" text="3" onClick={ this.handleClick } /></td>
+            <td rowSpan="2" ><Key id="equal" text="=" onClick={ this.handleOperatorClick } /></td>
           </tr>
           <tr>
-            <td colSpan="2" ><Key id="zero" text="0" /></td>
-            <td><Key id="dot" text="." /></td>
+            <td colSpan="2" ><Key id="zero" text="0" onClick={ this.handleClick }  /></td>
+            <td><Key id="dot" text="." onClick={ this.handleClick } /></td>
           </tr>
           </tbody>
         </table>
