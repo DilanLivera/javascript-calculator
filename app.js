d074915819367b59e32b243a7033b4b170b0e1f6
Dilan Livera <dylan_yahampath@yahoo.com>
2019-09-16 10:36:06 +1000

    add events for AC and = buttons and update styles

--- app.js
+++ app.js
@@ -11,32 +11,38 @@ class Calculator extends React.Component {
     super(props);
     this.state= {
       calculation: "0",
-      output: 0
+      output: 0,
+      justCalculated: false
     }
     this.handleClick = this.handleClick.bind(this);
     this.handleClearClick = this.handleClearClick.bind(this);
-    this.handleOperatorClick = this.handleOperatorClick.bind(this);
+    this.handleEqualClick = this.handleEqualClick.bind(this);
   }
 
-  handleClick(evt) {
-    let { calculation, output } = this.state;
-    calculation = (calculation === "0") ? evt.target.innerText : calculation + evt.target.innerText;
-    this.setState({ calculation, output });
+  handleClick(evt) {    
+    let { calculation,output, justCalculated } = this.state;
+    calculation = (justCalculated || calculation === "0") ? evt.target.innerText : calculation + evt.target.innerText;
+    if(justCalculated) {
+      output = "0";
+      justCalculated = false;
+    }
+    this.setState({ calculation, justCalculated, output });
   }
 
   handleClearClick() {
     let calculation = "0";
     let output = 0;
-    this.setState({ calculation, output });
+    let justCalculated = false;
+    this.setState({ calculation, output, justCalculated });
   }
 
-  handleOperatorClick(evt) {
-    const operator = evt.target.innerText;
-    if(operator === "+") console.log(operator);
-    if(operator === "-") console.log(operator); 
-    if(operator === "x") console.log(operator); 
-    if(operator === "/") console.log(operator);
-    if(operator === "=") console.log(operator);  
+  handleEqualClick() {
+    let calculation = this.state.calculation;
+    const reducer = (accumulator, currentValue) => (currentValue === "x") ? accumulator+"*" : accumulator+currentValue;
+    let newCalculation = calculation.split('').reduce(reducer, "");    
+    let output = eval(newCalculation);
+    const justCalculated = true;
+    this.setState({ calculation, output, justCalculated });
   }
 
   render() {
@@ -51,27 +57,27 @@ class Calculator extends React.Component {
         <table>
           <tbody>
           <tr>
-            <td colSpan="2" ><Key id="clear" text="clear" onClick={ this.handleClearClick } /></td>
-            <td><Key id="devide" text="/" onClick={ this.handleOperatorClick } /></td>
-            <td><Key id="multiply" text="x" onClick={ this.handleOperatorClick } /></td>
+            <td colSpan="2" ><Key id="clear" text="AC" onClick={ this.handleClearClick } /></td>
+            <td><Key id="devide" text="/" onClick={ this.handleClick } /></td>
+            <td><Key id="multiply" text="x" onClick={ this.handleClick } /></td>
           </tr>
           <tr>
             <td><Key id="seven" text="7" onClick={ this.handleClick } /></td>
             <td><Key id="eight" text="8" onClick={ this.handleClick } /></td>
             <td><Key id="nine" text="9" onClick={ this.handleClick } /></td>
-            <td><Key id="substract" text="-" onClick={ this.handleOperatorClick } /></td>
+            <td><Key id="substract" text="-" onClick={ this.handleClick } /></td>
           </tr>
           <tr>
             <td><Key id="four" text="4" onClick={ this.handleClick } /></td>
             <td><Key id="five" text="5" onClick={ this.handleClick } /></td>
             <td><Key id="six" text="6" onClick={ this.handleClick } /></td>
-            <td><Key id="add" text="+" onClick={ this.handleOperatorClick } /></td>
+            <td><Key id="add" text="+" onClick={ this.handleClick } /></td>
           </tr>
           <tr>
             <td><Key id="one" text="1" onClick={ this.handleClick } /></td>
             <td><Key id="two" text="2" onClick={ this.handleClick } /></td>
             <td><Key id="three" text="3" onClick={ this.handleClick } /></td>
-            <td rowSpan="2" ><Key id="equal" text="=" onClick={ this.handleOperatorClick } /></td>
+            <td rowSpan="2" ><Key id="equal" text="=" onClick={ this.handleEqualClick } /></td>
           </tr>
           <tr>
             <td colSpan="2" ><Key id="zero" text="0" onClick={ this.handleClick }  /></td>
